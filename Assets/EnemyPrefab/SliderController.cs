43450e1ea31e8c1ce913ccca3d06a93c1889f47d
annwaina4 <145358405+annwaina4@users.noreply.github.com>
2023-11-06 01:37:17 +0900

    更新
    ・ダメージ動作後に死亡動作をするように修正
    ・敵がプレーヤーを追いかける動作追加
    等

--- Assets/EnemyPrefab/SliderController.cs
+++ Assets/EnemyPrefab/SliderController.cs
@@ -4,42 +4,158 @@ using UnityEngine;
 
 public class SliderController : MonoBehaviour
 {
+    //状態（ステートパターン）
+    private int stateNumber = 0;
+
+    //汎用タイマー
+    private float timeCounter = 0f;
+
+    private Animator myanimator;
+
+    private Rigidbody myRigidbody;
+
+    private GameObject player;
+
     private int HP = 3;
-    // Start is called before the first frame update
+
+    //------------------------------------------------------------------------------------------------------------------
+    //スタート
+    //------------------------------------------------------------------------------------------------------------------
+
     void Start()
     {
-        
+        this.myanimator = GetComponent<Animator>();
+
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+        this.player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
+    //------------------------------------------------------------------------------------------------------------------
+    //オリジナル関数
+    //------------------------------------------------------------------------------------------------------------------
+
+    //距離を求める
+    float getLength(Vector3 current, Vector3 target)
+    {
+        return Mathf.Sqrt(((current.x - target.x) * (current.x - target.x)) + ((current.z - target.z) * (current.z - target.z)));
+    }
+
+    //方向を求める ※オイラー（-180～0～+180)
+    float getEulerAngle(Vector3 current, Vector3 target)
+    {
+        Vector3 value = target - current;
+        return Mathf.Atan2(value.x, value.z) * Mathf.Rad2Deg; //ラジアン→オイラー
+    }
+
+    //方向を求める ※ラジアン
+    float getRadian(Vector3 current, Vector3 target)
+    {
+        Vector3 value = target - current;
+        return Mathf.Atan2(value.x, value.z);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    //アップデート
+    //------------------------------------------------------------------------------------------------------------------
     void Update()
     {
-        
+        //タイマー加算
+        timeCounter += Time.deltaTime;
+
+        //方向を求める
+        float direction = getEulerAngle(this.transform.position, player.transform.position);
+
+        //距離を求める
+        float length = getLength(this.transform.position, player.transform.position);
+
+        //**************************************************************************************************************
+        //ここから状態処理
+        //**************************************************************************************************************
+
+        //待機
+        if (stateNumber == 0)
+        {
+            //プレーヤーの方向を向く
+            this.transform.rotation = Quaternion.Euler(0f, direction, 0f);
+
+            //1秒経過
+            if (timeCounter > 1.0f)
+            {
+                //タイマーリセット
+                timeCounter = 0f;
+
+                // アニメーション　前進
+                this.myanimator.SetFloat("speed", 1.0f);
+
+                //状態の遷移（前進）
+                stateNumber = 1;
+            }
+        }
+
+        //前進
+        else if (stateNumber == 1)
+        {
+            //プレーヤーの方向を向く
+            this.transform.rotation = Quaternion.Euler(0f, direction, 0f);
+
+            //移動
+            myRigidbody.velocity = transform.forward * 2.0f;
+
+            //5秒経過
+            if (timeCounter > 5.0f)
+            {
+                timeCounter = 0f;
+
+                //アニメーション　待機
+                this.myanimator.SetFloat("speed", 0);
+
+                //状態の遷移（待機）
+                stateNumber = 0;
+            }
+        }
+
     }
 
-    private void OnCollisionEnter(Collision collision)
+    //------------------------------------------------------------------------------------------------------------------
+    //衝突判定
+    //------------------------------------------------------------------------------------------------------------------
+    private void OnCollisionEnter(Collision other)
     {
-        //Debug.Log("OnCollisionEnter:" + collision.gameObject.tag);
-        Debug.Log("残りHP" + this.HP);
-        //ファイアーボール
-        if (collision.gameObject.tag == "Fireball" && HP > 0)
-        {
-            GetComponent<Animator>().SetTrigger("damage");
+        //Debug.Log("OnCollisionEnter:" + other.gameObject.tag);
 
-            HP--;         
+        //ファイアーボール
+        if (other.gameObject.tag == "Fireball" && HP > 0)
+        {
+            this.myanimator.SetTrigger("damage");
+
+            HP--;
         }
 
         //メテオ
-        if (collision.gameObject.tag == "Meteor" && HP > 0)
+        if (other.gameObject.tag == "Meteor" && HP > 0)
         {
-            GetComponent<Animator>().SetTrigger("damage");
+            this.myanimator.SetTrigger("damage");
 
             HP -= 3;
         }
 
+        //Debug.Log("残りHP" + this.HP);
+
         if (HP <= 0)
         {
-            GetComponent<Animator>().SetTrigger("death");
+            this.myanimator.SetBool("death", true);
+
+            //ステートパターンを停止
+            stateNumber = -1;
+
+            //自由落下を停止
+            myRigidbody.useGravity = false;
+            //衝突をなくす
+            GetComponent<CapsuleCollider>().enabled = false;
+
+            //５秒後に破棄
+            Destroy(this.gameObject, 5.0f);
         }
     }
 }
