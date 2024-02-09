7ff2d7baf543a98e83468ccc51861ccffdcf6ddb
annwaina4 <145358405+annwaina4@users.noreply.github.com>
2024-02-10 03:08:08 +0900

    更新

---
+++ Assets/Main/Enemy/EnemyGenerator.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Retro.ThirdPersonCharacter
+{
+    public class EnemyGenerator : MonoBehaviour
+    {
+        //敵の生成数
+        public int enemycounter = 0;
+
+        //敵の配列
+        public GameObject[] enemyprefab = new GameObject[4];
+
+        //生成場所の配列
+        public GameObject[] obeliskObject = new GameObject[4];
+        
+        //敵の生成頻度
+        float enemyTimecounter = 0f;
+        float span = 3.0f;
+
+        //回復アイテムの生成数
+        public int healCounter = 1;
+
+        float healTimeConter = 0f;
+
+        public GameObject Healprefab;
+        
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            enemyTimecounter += Time.deltaTime;
+            healTimeConter += Time.deltaTime;
+
+            if (Movement.isEnd == false)
+            {
+                //敵の生成
+                if (enemyTimecounter > span)
+                {
+                    enemyTimecounter = 0f;
+
+                    if (enemycounter <= 15)
+                    {
+                        //どの敵を生成するかの乱数
+                        int enemy = Random.Range(0, 4);
+
+                        //どの場所に生成するかの乱数
+                        int respawn = Random.Range(0, 4);
+
+                        Instantiate(enemyprefab[enemy], obeliskObject[respawn].transform.transform.position, Quaternion.identity);
+
+                        enemycounter++;
+                    }
+                }
+
+                //回復アイテムの生成
+                if (healTimeConter > 15.0f)
+                {
+                    healTimeConter = 0f;
+
+                    if (healCounter < 1)
+                    {
+                        GameObject healCopy = Instantiate(Healprefab, new Vector3(3.68f, -3.9f, -0.69f), Quaternion.identity);
+
+                        healCounter++;
+                    }
+                }
+            }
+            //Debug.Log("回復アイテムの数：" + healCounter);
+            //Debug.Log("healTimeConter：" + healTimeConter);
+        }
+    }
+}
---
+++ Assets/Main/Player/Scripts/Combat.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using NaughtyCharacter;
+
+namespace Retro.ThirdPersonCharacter
+{
+    [RequireComponent(typeof(PlayerInput))]
+    [RequireComponent(typeof(Animator))]
+    public class Combat : MonoBehaviour
+    {
+        private const string attackTriggerName = "Attack";
+        private const string specialAttackTriggerName = "Ability";
+
+        private Animator _animator;
+        private PlayerInput _playerInput;
+
+        public bool AttackInProgress {get; private set;} = false;
+
+        public GameObject fireballPrefab;
+
+        public GameObject meteorPrefab;
+
+        private GameObject childObj;
+
+        //private float speed = 300.0f;
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _playerInput = GetComponent<PlayerInput>();
+            childObj = transform.GetChild(1).gameObject;
+        }
+
+        private void Update()
+        {
+            if (Movement.isEnd == false)
+            {
+                if (_playerInput.AttackInput && !AttackInProgress)
+                {
+                    Attack();
+                    //GameObject fireball = (GameObject)Instantiate(fireballPrefab,childObj.transform.position,this.transform.rotation);
+                    //Rigidbody fireballRigidbody = fireball.GetComponent<Rigidbody>();
+                    //fireballRigidbody.AddForce(transform.forward *this.speed);
+                }
+                else if (_playerInput.SpecialAttackInput && !AttackInProgress)
+                {
+                    SpecialAttack();
+                    //メテオの生成
+                    GameObject Meteor = (GameObject)Instantiate(meteorPrefab, childObj.transform.position, this.transform.rotation);
+                }
+            }
+            //Debug.Log("AttackInProgress" + AttackInProgress);
+        }
+
+        private void SetAttackStart()
+        {
+            AttackInProgress = true;             
+        }
+
+        private void SetAttackEnd()
+        {
+            AttackInProgress = false;
+        }
+
+        private void Attack()
+        {
+            _animator.SetTrigger(attackTriggerName);
+        }
+
+        private void SpecialAttack()
+        {
+            _animator.SetTrigger(specialAttackTriggerName);
+        }
+
+        //ファイアーボールの生成
+        private void AttackEvent()
+        {
+            GameObject fireball = (GameObject)Instantiate(fireballPrefab, childObj.transform.position, this.transform.rotation);
+        }
+
+        public void AttackInProgressClear()
+        {
+            AttackInProgress = false;
+        }
+    }
+}
